bc821acdd61bb1cde429c04388ebb3f6af674b22
awtabani <55738683+awtabani@users.noreply.github.com>
2020-04-12 22:43:49 +0500

    Add files via upload

--- tests/acceptance/AppointmentsCest.php
+++ tests/acceptance/AppointmentsCest.php
@@ -2,6 +2,27 @@
 
 class AppointmentsCest
 {
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/user_login.php');
+        $I->fillField('username','admin');
+        $I->fillField('password','admin');
+        $I->click('Sign in');
+        
+    }
+        public function appointmentSuccessful(AcceptanceTester $I)
+        {
+            $I->amOnPage('/add_appointment.php');
+            $I->fillField('patient_id','150');
+            $I->fillField('date','2020-04-11');
+            $I->fillField('option_admission','daycase');
+            $I->fillField('time','2-3pm test');
+            $I->click('submit');
+            $I->click('submit');
+            $I->canSee('Appointment has been created');
+            $I->canSeeInCurrentUrl('/add_appointment.php');
+            
+        }
 
         public function appointmentEmptyName(AcceptanceTester $I)
         {
@@ -15,6 +36,7 @@ class AppointmentsCest
             $I->canSeeInCurrentUrl('/add_appointment.php');
             
         }
+
 
         public function appointmentEmptyDate(AcceptanceTester $I)
         {
@@ -37,19 +59,33 @@ class AppointmentsCest
             $I->fillField('option_admission','daycase');
             $I->fillField('time','');
             $I->click('submit');
-            $I->click('submit');
-            $I->canSee('Appointment has been created');
+            $I->canSee('Please fill in this field.');
             $I->canSeeInCurrentUrl('/add_appointment.php');
             
-
         }
 
-        public function appointmentCheckHTML(AcceptanceTester $I)
+
+        public function appointmentViewPage(AcceptanceTester $I)
         {
-            $I->seeElement('Appointment has been created');
-            $I->dontSeeElement('error');
-        }
+            $I->amOnPage('/appointments.php');
+            $I->see('Appointments');
+            $I->seeLink('ADD AN APPOINTMENT');
+            $I->see('Appointments confirmed by the patients');
+            $I->see('Patient Name');
+            $I->see('Date');
+            $I->see('Admission');
+            $I->see('Time');
+            $I->see('Manage');
+            $I->see('Appointments awaiting confirmation by the patients');
+            $I->see('Patient Name');
+            $I->see('Date');
+            $I->see('Admission');
+            $I->see('Time');
+            $I->see('Manage');
+            $I->canSeeInCurrentUrl('/appointments.php');
 
+        }
+        
 
     public function tryToTest(AcceptanceTester $I)
     {
