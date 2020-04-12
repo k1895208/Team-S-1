4e1d961832d07f52236c022d83a6d84140c72998
awtabani <55738683+awtabani@users.noreply.github.com>
2020-04-12 22:42:16 +0500

    Add files via upload

--- tests/acceptance/UserCest.php
+++ tests/acceptance/UserCest.php
@@ -3,7 +3,40 @@
 class UserCest
 
 {
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/user_login.php');
+        $I->fillField('username','admin');
+        $I->fillField('password','admin');
+        $I->click('Sign in');
+    }
     public function addUserEmpty(AcceptanceTester $I)
+    {
+        $I->wantTo("add an empty user");
+        $I->amOnPage('/addUser.php');
+        $I->fillField('username','');
+        $I->fillField('name','');
+        $I->fillField('surname','');
+        $I->fillField('email','');
+        $I->fillField('userLevel','');
+        $I->click('Submit Changes');
+        $I->seeCurrentUrlEquals('/Team-S/public/addUser.php');
+    }
+
+    public function addUserTestWorks(AcceptanceTester $I)
+    {
+        $I->wantTo("add a user");
+        $I->amOnPage('/addUser.php');
+        $I->fillField('username','hashim12');
+        $I->fillField('name','hashim');
+        $I->fillField('surname','amla');
+        $I->fillField('email','hashim@nhs.net');
+        $I->fillField('userLevel','1');
+        $I->click('Submit Changes');
+        $I->seeCurrentUrlEquals('/Team-S/public/users.php');
+    }
+
+    public function addEmptyUser(AcceptanceTester $I)
     {
         $I->wantTo("verify");
         $I->amOnPage('/addUser.php');
@@ -13,34 +46,9 @@ class UserCest
         $I->fillField('email','');
         $I->fillField('userLevel','');
         $I->click('Submit Changes');
-        $I->seeCurrentUrlEqual('users.php');
+        $I->seeCurrentUrlEquals('/Team-S/public/addUser.php');
     }
-
-    public function addUserTestWorks(AcceptanceTester $I)
-    {
-        $I->wantTo("verify");
-        $I->amOnPage('/addUser.php');
-        $I->fillField('username','hashim12');
-        $I->fillField('name','hashim');
-        $I->fillField('surname','amla');
-        $I->fillField('email','hashim@nhs.net');
-        $I->fillField('userLevel','1');
-        $I->click('Submit Changes');
-        $I->seeCurrentUrlEqual('users.php');
-    }
-
-    public function addUserTestTwo(AcceptanceTester $I)
-    {
-        $I->wantTo("verify");
-        $I->amOnPage('/addUser.php');
-        $I->fillField('username','');
-        $I->fillField('name','hashim');
-        $I->fillField('surname','');
-        $I->fillField('email','hashim@nhs.net');
-        $I->fillField('userLevel','60');
-        $I->click('Submit Changes');
-        $I->seeCurrentUrlEqual('users.php');
-    }
+    
 
     public function addUserTestThree(AcceptanceTester $I)
     {
@@ -52,7 +60,7 @@ class UserCest
         $I->fillField('email','hashim@yahoo.com');
         $I->fillField('userLevel','60');
         $I->click('Submit Changes');
-        $I->seeCurrentUrlEqual('users.php');
+        $I->seeCurrentUrlEquals('/Team-S/public/users.php');
     }
 
     public function editUserEmpty(AcceptanceTester $I)
@@ -65,20 +73,21 @@ class UserCest
         $I->fillField('email','');
         $I->fillField('userLevel','');
         $I->click('Submit Changes');
-        $I->seeCurrentUrlEqual('users.php');
+        $I->seeCurrentUrlEquals('/Team-S/public/editUser.php');
     }
 
     public function editUserTestWorks(AcceptanceTester $I)
     {
         $I->wantTo("verify");
-        $I->amOnPage('/editUser.php');
+        $I->amOnPage('/editUser.php?id=74');
         $I->fillField('username','testingyo');
         $I->fillField('name','again');
         $I->fillField('surname','testing');
         $I->fillField('email','nbaakza@kings.nhs.uk');
         $I->fillField('userLevel','3');
         $I->click('Submit');
-        $I->seeCurrentUrlEqual('users.php');
+        $I->seeCurrentUrlEquals('/Team-S/public/users.php');
+        $I->see('testingyo');
     }
 
      public function editUserTestTwo(AcceptanceTester $I)
@@ -91,7 +100,7 @@ class UserCest
         $I->fillField('email','nooriebaakza@yahoo.com');
         $I->fillField('userLevel','2');
         $I->click('Submit');
-        $I->seeCurrentUrlEqual('users.php');
+        $I->seeCurrentUrlEquals('/Team-S/public/editUser.php');
     }
 
 }
